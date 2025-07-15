cea3aed4aaceba7e5709edd8035fd711013664c0
Joao Augusto Ferreira Nunes de Carvalho <augusto.joao@discente.ufma.br>
2025-07-14 21:47:11 -0300

    refactor: ajustar valores padrão e limites de entrada para tipos de ônibus e rotas

--- frontend/pages/1_Criar_e_Otimizar.py
+++ frontend/pages/1_Criar_e_Otimizar.py
@@ -19,10 +19,10 @@ bus_types = []
 for i in range(num_types):
   with st.expander(f"Tipo de ônibus {i+1}"):
     tipo = {
-      "operational_cost": st.number_input("Custo operacional por km (R$)", min_value=0.0, key=f"custo_{i}"),
-      "capacity": st.number_input("Quantidade de assentos", min_value=1, key=f"assentos_{i}"),
-      "available_units": st.number_input("Quantidade de ônibus disponíveis", min_value=0, key=f"qtd_{i}"),
-      "load_factor": st.number_input("Fator máximo de ocupação", min_value=1.0, key=f"fator_{i}")
+      "operational_cost": st.number_input("Custo operacional por km (R$)", min_value=0.0, value=1.0, key=f"custo_{i}"),
+      "capacity": st.number_input("Quantidade de assentos", min_value=1, value=50, step=50, key=f"assentos_{i}"),
+      "available_units": st.number_input("Quantidade de ônibus disponíveis", min_value=0, value=10, step=10, key=f"qtd_{i}"),
+      "load_factor": st.number_input("Fator máximo de ocupação", min_value=1.0, value=1.0, step=0.1, key=f"fator_{i}")
     }
     bus_types.append(tipo)
 
@@ -31,18 +31,18 @@ routes = []
 for i in range(num_routes):
   with st.expander(f"Rota {i+1}"):
     route = {
-      "length_km": st.number_input("Comprimento da rota (km)", min_value=0.10, step=0.10,key=f"comprimento_{i}"),
+      "length_km": st.number_input("Comprimento da rota (km)", min_value=0.50, step=0.50,key=f"comprimento_{i}"),
       "time_minutes": st.number_input("Tempo de viagem (minutos)", min_value=1, key=f"tempo_{i}"),
-      "passengers": st.number_input("Número de passageiros", min_value=1, key=f"passageiros_{i}")
+      "passengers": st.number_input("Número de passageiros", min_value=1, value=50, step=50, key=f"passageiros_{i}")
     }
     routes.append(route)
 
 # Sobre os parametros
 parameters = dict()
-parameters["time_unit_of_periods"] = st.number_input("Tempo do período (minutos)", min_value=1, step=1)
+parameters["time_unit_of_periods"] = st.number_input("Tempo do período (minutos)", min_value=0, value=60, step=1)
 parameters["accetable_time_transfer"] = st.number_input("Tempo aceitável de transferência (minutos)", min_value=0, step=1)
-parameters["agglomeration_cost"] = st.number_input("Custo de aglomeração", min_value=1.0, step=0.5)
-parameters["accetable_time_transfer"] = st.number_input("Custo de tempo de espera", min_value=1.0, step=0.5)
+parameters["agglomeration_cost"] = st.number_input("Custo de aglomeração", min_value=0.0, value=1.0, step=0.5)
+parameters["accetable_time_transfer"] = st.number_input("Custo de tempo de espera", min_value=0.0, value=1.0, step=0.5)
 
 if st.button("Otimizar"):
   response = requests.post(
